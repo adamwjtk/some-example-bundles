539679a22d3074ba98b022d639d3c7a5bf5477fe
AdamWo <a.wojtkiewicz@jarkom.pl>
2018-08-09 23:55:25 +0200

    rest api routing

---
+++ src/Adamwjtk/ProductBundle/Service/Create/ProductFieldsSet.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AdamwjtkProductBundle\Service\Create;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductFieldsSet
+{
+
+    public $status = false;
+
+    /**
+     * @return bool
+     */
+    public function isStatus(): bool
+    {
+        return $this->status;
+    }
+
+    private $name;
+
+    /**
+     * @return null|string
+     */
+    public function getName():?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    private function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getAmount():?string
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param int $amount
+     */
+    private function setAmount(int $amount)
+    {
+        $this->amount = $amount;
+    }
+    private $amount;
+
+    public function setFields(Request $post){
+
+        if($post->get('name') && $post->get('amount')){
+            $this->setName($post->get('name'));
+            $this->setAmount($post->get('amount'));
+            $this->status = true;
+        }
+    }
+}
